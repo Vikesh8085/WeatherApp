7d0c26d40dba6320c7247655a6e8b1280bbb1a90
Vikesh <vikesh8085@gmail.com>
2022-01-16 16:59:18 +0530

    Date format changed for sunrise and sunset

--- Weather App/Helper/Extenstion/Int + Extension.swift
+++ Weather App/Helper/Extenstion/Int + Extension.swift
@@ -21,4 +21,13 @@ extension Int {
     func getVisibity() -> String {
         return String(self / 1000) + " km"
     }
+    
+    func setupData() -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self))
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.medium //Set time style
+        dateFormatter.timeZone = .current
+        let localDate = dateFormatter.string(from: date)
+        return localDate
+    }
 }
--- Weather App/View/WeatherList View/Cell/WeatherListHeaderView.swift
+++ Weather App/View/WeatherList View/Cell/WeatherListHeaderView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 final class WeatherListHeaderView: UITableViewHeaderFooterView {
-
+    
     @IBOutlet weak var lblCityName: UILabel?
     @IBOutlet weak var lblLat: UILabel?
     @IBOutlet weak var lblLong: UILabel?
@@ -20,16 +20,25 @@ final class WeatherListHeaderView: UITableViewHeaderFooterView {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
- 
+    
     // MARK: - Private methods
-
+    
     func setData(data: City?) {
+        if let date = data?.sunrise?.setupData() {
+            lblSunRiseTime?.text = date
+        } else {
+            lblSunRiseTime?.text = "No Date"
+        }
+        
+        if let date = data?.sunset?.setupData() {
+            lblSunset?.text = date
+        } else {
+            lblSunset?.text = "No Date"
+        }
+        
         self.lblCityName?.text = "\(data?.name ?? ""),\(data?.country ?? "")"
         self.lblLat?.text = "Lat: \(data?.coord?.lat ?? 0)"
         self.lblLong?.text = "Long: \(data?.coord?.lon ?? 0)"
-        self.lblSunRiseTime?.text = "\(data?.sunrise ?? 0)"
-        self.lblSunset?.text = "\(data?.sunset ?? 0)"
-
     }
-
+    
 }
